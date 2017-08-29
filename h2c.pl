f98a65caac0236e9715f56d02923062606a747c5
Daniel Stenberg <daniel@haxx.se>
2017-08-29 09:23:22 +0200

    support -s for short options

--- h2c.pl
+++ h2c.pl
@@ -1,5 +1,28 @@
 #!/usr/bin/perl
 
+sub usage {
+    print "h2c.pl [-s] < file \n",
+        " -h   Show short help\n";
+        " -s   Use short command line options\n";
+    exit;
+}
+
+my $usesamehttpversion = 1;
+my $disableheadersnotseen = 1;
+my $shellcompatible = 1; # may not been windows command prompt compat
+my $uselongoptions = 1; # instead of short
+
+while($ARGV[0]) {
+    if(($ARGV[0] eq "-h") || ($ARGV[0] eq "--help")) {
+        usage();
+    }
+    if($ARGV[0] eq "-s") {
+        $uselongoptions = 0;
+        shift @ARGV;
+    }
+}
+
+
 my $state; # 0 is request-line, 1-headers, 2-body
 while(<STDIN>) {
     my $l = $_;
@@ -33,10 +56,6 @@ while(<STDIN>) {
     }
 }
 
-my $usesamehttpversion = 1;
-my $disableheadersnotseen = 1;
-my $shellcompatible = 1; # may not been windows command prompt compat
-
 if(!$header{lc('Host')}) {
     $error = "No Host: header makes it impossible to tell URL\n";
 }
@@ -45,6 +64,23 @@ if(!$header{lc('Host')}) {
 if($error) {
     print "Error: $error\n";
     exit;
+}
+
+if($uselongoptions) {
+    $opt_data = "--data";
+    $opt_request = "--request";
+    $opt_head = "--head";
+    $opt_header = "--header";
+    $opt_user_agent = "--user-agent";
+    $opt_cookie = "--cookie";
+}
+else {
+    $opt_data = "-d";
+    $opt_request = "-X";
+    $opt_head = "-I";
+    $opt_header = "-H";
+    $opt_user_agent = "-A";
+    $opt_cookie = "-b";
 }
 
 my $httpver="";
@@ -60,18 +96,18 @@ if(length(join("", @body))) {
     $usebody= sprintf("--data-binary \"%s\" ", $esc);
 }
 if(uc($method) eq "HEAD") {
-    $usemethod = "--head ";
+    $usemethod = "$opt_head ";
 }
 elsif(uc($method) eq "POST") {
     if(!$usebody) {
-        $usebody= sprintf("--data \"\" ");
+        $usebody= sprintf("$opt_data \"\" ");
     }
 }
 elsif(uc($method) eq "PUT") {
     if(!$usebody) {
-        $usebody= sprintf("--data \"\"");
+        $usebody= sprintf("$opt_data \"\"");
     }
-    $usebody .= "--request PUT ";
+    $usebody .= "$opt_request PUT ";
 }
 elsif(uc($method) ne "GET") {
     $error = "unsupported HTTP method $method";
@@ -93,10 +129,10 @@ if($usesamehttpversion) {
 }
 if($disableheadersnotseen) {
     if(!$header{lc('Accept')}) {
-        $disabledheaders .= "--header Accept: ";
+        $disabledheaders .= "$opt_header Accept: ";
     }
     if(!$header{lc('User-Agent')}) {
-        $disabledheaders .= "--header User-Agent: ";
+        $disabledheaders .= "$opt_header User-Agent: ";
     }
 }
 foreach my $h (keys %header) {
@@ -107,12 +143,12 @@ foreach my $h (keys %header) {
         # we don't set custom size, just usebody
     }
     else {
-        my $opt = sprintf("--header \"%s: ", $h);
+        my $opt = sprintf("$opt_header \"%s: ", $h);
         if(lc($h) eq "user-agent") {
-            $opt = "--user-agent \"";
+            $opt = "$opt_user_agent \"";
         }
         elsif(lc($h) eq "cookie") {
-            $opt = "--cookie \"";
+            $opt = "$opt_cookie \"";
         }
         $addedheaders .= sprintf("%s%s\" ", $opt, $header{$h});
     }
